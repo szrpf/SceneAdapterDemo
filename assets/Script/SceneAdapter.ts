ebd0bc230573ae091bfdc09585d6612053f65d5d
szrpf <27185709@qq.com>
2023-07-06 12:02:39 +0800

    v1.0

---
+++ assets/Script/SceneAdapter.ts
@@ -0,0 +1,25 @@
+const { ccclass, menu } = cc._decorator;
+
+@ccclass
+@menu('Comp/SceneAdapter')
+export default class SceneAdapter extends cc.Component {
+
+    protected onLoad() {
+        this.resize();
+        cc.view.setResizeCallback(this.resize.bind(this));
+    }
+
+    private resize() {
+        let node = this.node;
+        if (cc.sys.isMobile) {
+            node.width = cc.winSize.width;
+            node.height = cc.winSize.height;
+        } else {
+            if (cc.winSize.width / cc.winSize.height > node.width / node.height) {
+                node.scale = cc.winSize.height / node.height;
+            } else {
+                node.scale = cc.winSize.width / node.width;
+            }
+        }
+    }
+}
