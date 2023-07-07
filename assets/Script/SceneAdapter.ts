c4d0f72e5f1d6e0bad86e315da2ec653f84d76dd
szrpf <27185709@qq.com>
2023-07-07 12:41:00 +0800

    v1.1

--- assets/Script/SceneAdapter.ts
+++ assets/Script/SceneAdapter.ts
@@ -1,5 +1,11 @@
+/*******************************************************************************
+ * 创建: 2023年07月02日
+ * 作者: 水煮肉片饭(27185709@qq.com)
+ * 描述: 场景适配器
+ *      给场景Scene根节点（Canvas组件所在的节点）挂上SceneAdapter
+ *      就可以自动适配屏幕尺寸
+*******************************************************************************/
 const { ccclass, menu } = cc._decorator;
-
 @ccclass
 @menu('Comp/SceneAdapter')
 export default class SceneAdapter extends cc.Component {
